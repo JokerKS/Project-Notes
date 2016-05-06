36cb247b35f3b0adaa77bfdd999d2d5cca3d379f
Joker_KS <kozenkovsktor@mail.ru>
2016-05-06 15:25:28 +0200

    tray

--- NotesProject/MainWindow.xaml.cs
+++ NotesProject/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace Project
             InitializeComponent();
             InitMainMenu();
             InitializeNotatnik();
+            createTrayIcon();
         }
 
 
@@ -472,5 +473,121 @@ namespace Project
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon TrayIcon = null;
+        private ContextMenu TrayMenu = null;
+
+        private bool createTrayIcon()
+        {
+            bool result = false;
+            if (TrayIcon == null)
+            { // только если мы не создали иконку ранее
+                TrayIcon = new System.Windows.Forms.NotifyIcon(); // создаем новую
+                TrayIcon.Icon = NotesProject.Properties.Resources.TreyIcon; // изображение для трея
+                                                                            // обратите внимание, за ресурсом с картинкой мы лезем в свойства проекта, а не окна,
+                                                                            // поэтому нужно указать полный namespace
+                TrayIcon.Text = "Ku-ku"; // текст подсказки, всплывающей над иконкой
+                TrayMenu = Resources["TrayMenu"] as ContextMenu; // а здесь уже ресурсы окна и тот самый x:Key
+
+                // сразу же опишем поведение при щелчке мыши, о котором мы говорили ранее
+                // это будет просто анонимная функция, незачем выносить ее в класс окна
+                TrayIcon.Click += delegate (object sender, EventArgs e)
+                {
+                    if ((e as System.Windows.Forms.MouseEventArgs).Button == System.Windows.Forms.MouseButtons.Left)
+                    {
+                        // по левой кнопке показываем или прячем окно
+                        ShowHideMainWindow(sender, null);
+                    }
+                    else {
+                        // по правой кнопке (и всем остальным) показываем меню
+                        TrayMenu.IsOpen = true;
+                        Activate(); // нужно отдать окну фокус, см. ниже
+                    }
+                };
+                result = true;
+            }
+            else { // все переменные были созданы ранее
+                result = true;
+            }
+            TrayIcon.Visible = true; // делаем иконку видимой в трее
+            return result;
+        }
+
+        private void ShowHideMainWindow(object sender, RoutedEventArgs e)
+        {
+            TrayMenu.IsOpen = false; // спрячем менюшку, если она вдруг видима
+            if (IsVisible)
+            {
+                Hide();
+                (TrayMenu.Items[0] as MenuItem).Header = "Pokaż";
+            }
+            else
+            { 
+                Show();
+                (TrayMenu.Items[0] as MenuItem).Header = "Ukryj";
+                WindowState = CurrentWindowState;
+                Activate(); // обязательно нужно отдать фокус окну,
+                            // иначе пользователь сильно удивится, когда увидит окно
+                            // но не сможет в него ничего ввести с клавиатуры
+            }
+        }
+
+        private System.Windows.WindowState fCurrentWindowState = System.Windows.WindowState.Normal;
+        public System.Windows.WindowState CurrentWindowState
+        {
+            get { return fCurrentWindowState; }
+            set { fCurrentWindowState = value; }
+        }
+
+        // переопределяем встроенную реакцию на изменение состояния сознания окна
+        protected override void OnStateChanged(EventArgs e)
+        {
+            base.OnStateChanged(e); // системная обработка
+            if (this.WindowState == System.Windows.WindowState.Minimized)
+            {
+                // если окно минимизировали, просто спрячем
+                Hide();
+                // и поменяем надпись на менюшке
+                (TrayMenu.Items[0] as MenuItem).Header = "Pokaż";
+            }
+            else {
+                // в противном случае запомним текущее состояние
+                CurrentWindowState = WindowState;
+            }
+        }
+
+        private bool fCanClose = false;
+        public bool CanClose
+        { // флаг, позволяющий или запрещающий выход из приложения
+            get { return fCanClose; }
+            set { fCanClose = value; }
+        }
+
+        // переопределяем обработчик запроса выхода из приложения
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            base.OnClosing(e); // встроенная обработка
+            if (!CanClose)
+            {    // если нельзя закрывать
+                e.Cancel = true;
+                //выставляем флаг отмены закрытия
+              // запоминаем текущее состояние окна
+                CurrentWindowState = this.WindowState;
+                // меняем надпись в менюшке
+                (TrayMenu.Items[0] as MenuItem).Header = "Pokaż";
+                // прячем окно
+                Hide();
+            }
+            else { // все-таки закрываемся
+                   // убираем иконку из трея
+                TrayIcon.Visible = false;
+            }
+        }
+
+        private void MenuExitClick(object sender, RoutedEventArgs e)
+        {
+            CanClose = true;
+            Close();
+        }
     }
 }
